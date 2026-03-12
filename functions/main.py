6b1b2ef45d6fbba8c50497b1e5f16351163db894
Vidura <viduranip.15@gmail.com>
2026-03-12 18:07:56 +0530

    Configured Firebase Local Emulators

--- functions/main.py
+++ functions/main.py
@@ -1,21 +1,36 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
+from firebase_functions import firestore_fn
+from firebase_admin import initialize_app, firestore
+from google.cloud.firestore import Increment
 
-from firebase_functions import https_fn
-from firebase_functions.options import set_global_options
-from firebase_admin import initialize_app
+# Wakes up the Firebase admin privileges so we can edit the database
+initialize_app()
 
-# For cost control, you can set the maximum number of containers that can be
-# running at the same time. This helps mitigate the impact of unexpected
-# traffic spikes by instead downgrading performance. This limit is a per-function
-# limit. You can override the limit for each function using the max_instances
-# parameter in the decorator, e.g. @https_fn.on_request(max_instances=5).
-set_global_options(max_instances=10)
+# This tells Google's servers to listen for ANY new document added to 'patient_logs'
+@firestore_fn.on_document_created(document="patient_logs/{logId}")
+def update_dengue_heatmap(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -> None:
+    
+    # If there is no data, stop the function
+    if event.data is None:
+        return
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+    # Grab the data from the newly submitted log
+    patient_data = event.data.to_dict()
+    disease = patient_data.get("disease")
+    district = patient_data.get("district")
+
+    # Check if the disease is Dengue and a district was provided
+    if disease == "Dengue" and district:
+        
+        # Connect to your Firestore database
+        db = firestore.client()
+        
+        # Point directly to your Dengue scoreboard
+        stats_ref = db.collection("heatmap_statistics").document("Dengue")
+        
+        # Increment that specific district's tally by exactly 1
+        # merge=True ensures it doesn't overwrite your other districts
+        stats_ref.set({
+            district: Increment(1)
+        }, merge=True)
+        
+        print(f"Successfully added 1 to {district} for Dengue!")
